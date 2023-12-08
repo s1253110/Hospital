188f20937fdc20e43c5cd0707f09ec543dca8644
s1253110 <144907957+s1253110@users.noreply.github.com>
2023-12-08 13:38:51 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+<!-- index.ejs -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Options</title>
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="/find" target="_blank">Find</a></li>
+      <li><a href="/create" target="_blank">Create</a></li>
+      <li><a href="/update" target="_blank">Update</a></li>
+    </ul>
+  </nav>
+</body>
+</html>
